db0418d48ba4b6512e39f7de46ef94984bc32612
sakup <pukasbohdan@gmail.com>
2016-11-14 15:26:06 +0200

    a lot of changes

--- dbase.py
+++ dbase.py
@@ -1,3 +1,5 @@
+import kivy
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
@@ -14,16 +16,49 @@ class StudentDB(BoxLayout):
     student_list = ObjectProperty()
 
     def submit_student(self):
-        pass
+        # Get the student name from TextInput
+        student_name = self.first_name_text_input.text + " " + self.last_name_text_input.text
+
+        # Add to listview
+        self.student_list.adapter.data.extend([student_name])
+
+        # Reset the listview
+        self.student_list._trigger_reset_populate()
 
     def delete_student(self):
-        pass
+        # If a list item is selected
+        if self.student_list.adapter.selection:
+
+            # Get the text from the item selected
+            selection = self.student_list.adapter.selection[0].text
+
+            # Remove the matching item
+            self.student_list.adapter.data.remove(selection)
+
+            # Reset the listview
+            self.student_list._trigger_reset_populate()
 
     def replase_student(self):
-        pass
+        # If a list item is selected
+        if self.student_list.adapter.selection:
 
-class StudentDBApp(App):
+            # Get the text from the item selected
+            selection = self.student_list.adapter.selection[0].text
+
+            # Remove the matching item
+            self.student_list.adapter.data.remove(selection)
+
+            # Get the student name from TextInput
+            student_name = self.first_name_text_input.text + " " + self.last_name_text_input.text
+
+            # Add the updated data to list
+            self.student_list.adapter.data.extend([student_name])
+
+            # Reset the ListView
+            self.student_list._trigger_reset_populate()
+
+class DbaseApp(App):
     def build(self):
-        return StudentDbase()
+        return StudentDB()
 
-StudentDBApp().run()
+DbaseApp().run()
